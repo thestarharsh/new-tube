d4ce57b297b4321a88229b919db20051cfd40f87
Harsh Jain <talatiharsh19@gmail.com>
2025-12-02 14:48:03 +0530

    Added rate limiting and auth protection to tRPC endpoints.

--- src/trpc/client.tsx
+++ src/trpc/client.tsx
@@ -1,5 +1,6 @@
 'use client';
 // ^-- to make sure we can mount the Provider from a server component
+import superjson from 'superjson';
 import type { QueryClient } from '@tanstack/react-query';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { httpBatchLink } from '@trpc/client';
@@ -41,8 +42,13 @@ export function TRPCProvider(
     trpc.createClient({
       links: [
         httpBatchLink({
-          // transformer: superjson, <-- if you use a data transformer
+          transformer: superjson,
           url: getUrl(),
+          async headers() {
+            const headers = new Headers();
+            headers.set('x-trpc-source', 'nextjs-react');
+            return headers;
+          },
         }),
       ],
     }),
